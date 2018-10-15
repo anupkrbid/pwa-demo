ae0587a829a765fd1f9a15aaca9028c084d9da87
anupkrbid <anupbid94@gmail.com>
2018-10-16 02:14:32 +0530

    Implemented Service Worker Caching

--- public/sw.js
+++ public/sw.js
@@ -1,17 +1,61 @@
+var CACHE_STATIC_NAME = 'static-v1';
+var CACHE_DYNAMIC_NAME = 'dynamic-v1';
+
 self.addEventListener('install', function(event) {
-  console.log('[SW] ~ Installing Service Worker ...', event);
+  console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+    caches.open(CACHE_STATIC_NAME).then(function(cache) {
+      console.log('[Service Worker] Pre Caching App Shell ...');
+      cache.addAll([
+        '/',
+        '/index.html',
+        '/src/js/app.js',
+        '/src/js/feed.js',
+        '/src/js/lib/fetch.js',
+        '/src/js/lib/promise.js',
+        '/src/js/lib/material.min.js',
+        '/src/css/app.css',
+        '/src/css/feed.css',
+        '/src/images/main-image.jpg',
+        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+        'https://fonts.googleapis.com/icon?family=Material+Icons'
+      ]);
+    })
+  );
 });
 
 self.addEventListener('activate', function(event) {
-  console.log('[SW] ~ Activating Service Worker ...', event);
+  console.log('[Service Worker] Activating Service Worker ....', event);
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(
+        keyList.map(function(key) {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            console.log('[Service Worker] Revoming Old Cache ....', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
   return self.clients.claim();
 });
 
-self
-  .addEventListener('fetch', function(event) {
-    console.log('[SW] ~ Fetching assets ...', event);
-    event.respondWith(fetch(event.request));
-  })
-  .catch(function(err) {
-    console.log('[SW] ~ Error while Fetching assets ...', err);
-  });
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.request)
+          .then(function(res) {
+            caches.open('dynamic').then(function(cache) {
+              cache.put(event.request.url, res.clone());
+              return res;
+            });
+          })
+          .catch(function(err) {});
+      }
+    })
+  );
+});
