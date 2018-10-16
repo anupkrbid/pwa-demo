1acf983b886d355134cfac0d72aff29e6bd55309
anupkrbid <anupbid94@gmail.com>
2018-10-17 04:26:39 +0530

    Implemented Different Caching Stratergies

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -1,3 +1,4 @@
+// var CACHE_USER_REQUESTED_NAME = 'user-requested-v1';
 var shareImageButton = document.querySelector('#share-image-button');
 var createPostArea = document.querySelector('#create-post');
 var sharedMomentsArea = document.querySelector('#shared-moments');
@@ -29,6 +30,12 @@ shareImageButton.addEventListener('click', openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
 
+function clearCard() {
+  while (sharedMomentsArea.firstChild) {
+    sharedMomentsArea.removeChild(sharedMomentsArea.firstChild);
+  }
+}
+
 function createCard() {
   var cardWrapper = document.createElement('div');
   cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
@@ -47,15 +54,47 @@ function createCard() {
   cardSupportingText.className = 'mdl-card__supporting-text';
   cardSupportingText.textContent = 'In San Francisco';
   cardSupportingText.style.textAlign = 'center';
+  // var cardSaveButton = document.createElement('button');
+  // cardSaveButton.textContent = 'Save';
+  // cardSaveButton.addEventListener('click', onSaveCardHandler);
+  // cardSupportingText.appendChild(cardSaveButton);
   cardWrapper.appendChild(cardSupportingText);
   componentHandler.upgradeElement(cardWrapper);
   sharedMomentsArea.appendChild(cardWrapper);
 }
 
-fetch('https://httpbin.org/get')
+var url = 'https://httpbin.org/get';
+var networkDataReceived = false;
+fetch(url)
   .then(function(res) {
     return res.json();
   })
   .then(function(data) {
+    networkDataReceived = true;
+    clearCard();
     createCard();
   });
+
+if ('caches' in window) {
+  caches
+    .match(url)
+    .then(function(res) {
+      return res.json();
+    })
+    .then(function(data) {
+      if (!networkDataReceived) {
+        clearCard();
+        createCard();
+      }
+    });
+}
+
+// Currently not in use, Allows to cache data on demand
+function onSaveCardHandler(event) {
+  if ('caches' in window) {
+    caches.open(CACHE_USER_REQUESTED_NAME).then(function(cache) {
+      console.log('[Feed.js] Caching User Request ...');
+      cache.addAll(['/src/images/sf-boat.jpg', 'https://httpbin.org/get']);
+    });
+  }
+}
--- public/sw.js
+++ public/sw.js
@@ -1,25 +1,27 @@
 var CACHE_STATIC_NAME = 'static-v1';
 var CACHE_DYNAMIC_NAME = 'dynamic-v1';
+var STATIC_FILES = [
+  '/',
+  '/index.html',
+  '/offline.html',
+  '/src/js/app.js',
+  '/src/js/feed.js',
+  '/src/js/lib/fetch.js',
+  '/src/js/lib/promise.js',
+  '/src/js/lib/material.min.js',
+  '/src/css/app.css',
+  '/src/css/feed.css',
+  '/src/images/main-image.jpg',
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+  'https://fonts.googleapis.com/icon?family=Material+Icons'
+];
 
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
   event.waitUntil(
     caches.open(CACHE_STATIC_NAME).then(function(cache) {
       console.log('[Service Worker] Pre Caching App Shell ...');
-      cache.addAll([
-        '/',
-        '/index.html',
-        '/src/js/app.js',
-        '/src/js/feed.js',
-        '/src/js/lib/fetch.js',
-        '/src/js/lib/promise.js',
-        '/src/js/lib/material.min.js',
-        '/src/css/app.css',
-        '/src/css/feed.css',
-        '/src/images/main-image.jpg',
-        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
-        'https://fonts.googleapis.com/icon?family=Material+Icons'
-      ]);
+      cache.addAll(STATIC_FILES);
     })
   );
 });
@@ -41,21 +43,115 @@ self.addEventListener('activate', function(event) {
   return self.clients.claim();
 });
 
+// Cache then Network Stratergy combined with Custom Stratergy and Cache Only Stratergy
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      if (response) {
-        return response;
-      } else {
-        return fetch(event.request)
-          .then(function(res) {
-            caches.open('dynamic').then(function(cache) {
-              cache.put(event.request.url, res.clone());
-              return res;
+  var url = 'https://httpbin.org/get';
+  if (event.request.url.indexOf(url) > -1) {
+    // Cache then network Stratergy
+    event.respondWith(
+      caches.open(CACHE_DYNAMIC_NAME).then(function(cache) {
+        return fetch(event.request).then(function(res) {
+          cache.put(event.request.url, res.clone());
+          return res;
+        });
+      })
+    );
+  } else if (
+    new RegExp('\\b' + STATIC_FILES.join('\\b|\\b') + '\\b').test(
+      event.request.url
+    )
+  ) {
+    // Cache Only Stratergy
+    return event.respondWith(caches.match(event.request));
+  } else {
+    // Custom Stratergy
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else {
+          return fetch(event.request)
+            .then(function(res) {
+              caches.open(CACHE_DYNAMIC_NAME).then(function(cache) {
+                cache.put(event.request.url, res.clone());
+                return res;
+              });
+            })
+            .catch(function(err) {
+              return caches.open(CACHE_STATIC_NAME).then(function(cache) {
+                if (event.request.url.indexOf('/help') > -1) {
+                  return caches.match('offline.html');
+                }
+              });
             });
-          })
-          .catch(function(err) {});
-      }
-    })
-  );
+        }
+      })
+    );
+  }
 });
+
+// Custom Stratergy
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request).then(function(response) {
+//       if (response) {
+//         return response;
+//       } else {
+//         return fetch(event.request)
+//           .then(function(res) {
+//             caches.open(CACHE_DYNAMIC_NAME).then(function(cache) {
+//               cache.put(event.request.url, res.clone());
+//               return res;
+//             });
+//           })
+//           .catch(function(err) {
+//             return caches.open(CACHE_STATIC_NAME).then(function(cache) {
+//               return caches.match('offline.html');
+//             });
+//           });
+//       }
+//     })
+//   );
+// });
+
+// Cache then Network Stratergy
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.open(CACHE_DYNAMIC_NAME).then(function(cache) {
+//       return fetch(event.request).then(function(res) {
+//         cache.put(event.request.url, res.clone());
+//         return res;
+//       });
+//     })
+//   );
+// });
+
+// Network with Cache Fallback Stratergy
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     fetch(event.request)
+//       .then(function(res) {
+//         caches.open(CACHE_DYNAMIC_NAME).then(function(cache) {
+//           cache.put(event.request.url, res.clone());
+//           return res;
+//         });
+//       })
+//       .catch(function(err) {
+//         caches.match(event.request).catch(function(err) {
+//           return caches.open(CACHE_STATIC_NAME).then(function(cache) {
+//             return caches.match('offline.html');
+//           });
+//         });
+//       })
+//   );
+// });
+
+// Cache Only Stratergy
+// self.addEventListener('fetch', function(event) {
+//   return event.respondWith(caches.match(event.request));
+// });
+
+// Network Only Stratergy
+// self.addEventListener('fetch', function(event) {
+//   return event.respondWith(event.request);
+// });
